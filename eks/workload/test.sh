57598fa31d5b7d63120d36b7e5d8ba365ca9e696
DrumSergio <sergio1993_1@hotmail.com>
2022-01-10 23:25:55 +0100

    Add results well GKE

--- eks/workload/test.sh
+++ eks/workload/test.sh
@@ -3,12 +3,12 @@ kubectl delete -k ./
 
 echo "Deleted resources"
 
-velero restore create my-backup-$RANDOM --from-backup velero-wm-daily-20220109143912
+velero restore create $RANDOM-my-backup-$RANDOM --from-backup velero-wm-daily-20220109143912
 
 echo "Velero backup restored"
 #sleep 20 >/dev/null # No problem of waiting 10s... the kubernetes deployment still continues underneath
 
-until  kubectl wait deploy/wordpress --timeout=300s --for=condition=available;
+until kubectl wait deploy/wordpress --timeout=300s --for=condition=available;
 do
   sleep 1;
 done;
--- gke/workload/test.sh
+++ gke/workload/test.sh
@@ -3,9 +3,12 @@ kubectl delete -k ./ >/dev/null
 
 echo "Deleted resources" >/dev/null
 
-velero restore create my-backup-$RANDOM --from-backup test-wp >/dev/null
+velero restore create $RANDOM-my-backup-$RANDOM --from-backup test-wp >/dev/null
 
 echo "Velero backup restored" >/dev/null
-sleep 10 >/dev/null # No problem of waiting 10s... the kubernetes deployment still continues underneath
+#sleep 10 >/dev/null # No problem of waiting 10s... the kubernetes deployment still continues underneath
 
-kubectl wait deploy/wordpress --timeout=300s --for=condition=available >/dev/null
+until kubectl wait deploy/wordpress --timeout=300s --for=condition=available; >/dev/null
+do
+  sleep 1;
+done;
